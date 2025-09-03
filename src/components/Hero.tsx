2e6d53858ba8a04ab519f7c1455400e8e2d17d70
Emir Risyad <emirrsyd@gmail.com>
2025-09-03 18:57:40 +0200

    feat: update hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,12 +1,15 @@
 export function Hero() {
   return (
-    <div className=' flex items-center justify-center'>
-      <div className='absolute'>
+    <div className='min-h-screen flex items-center justify-center relative'>
+      <div className='relative'>
         <div className='w-[500px] h-[500px] rounded-full border-8 border-golden-rod-yellow bg-transparent flex items-center justify-center animate-pulse-scale'></div>
         {/* Text overlay */}
-        <div className='absolute inset-0 flex items-center justify-center'>
-          <h1 className='text-8xl font-black text-white tracking-widest whitespace-nowrap'>
-            EMIR RISYAD
+        <div className='absolute inset-0 gap-8 flex items-center justify-center'>
+          <h1 className='font-light font-montserrat text-8xl text-white tracking-wide whitespace-nowrap'>
+            EMIR
+          </h1>
+          <h1 className='font-bold font-montserrat text-8xl text-white tracking-wide whitespace-nowrap'>
+            RISYAD
           </h1>
         </div>
       </div>
